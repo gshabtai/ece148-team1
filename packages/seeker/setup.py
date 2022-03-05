4e184f8b9ad6ce47f6436c22bdc494c5c7481afb
Parker <pknopf@ucsd.edu>
2022-03-05 13:14:59 -0800

    launch file share

--- packages/seeker/setup.py
+++ packages/seeker/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from glob import glob
+import os
 
 package_name = 'seeker'
 
@@ -9,6 +11,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+            (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
