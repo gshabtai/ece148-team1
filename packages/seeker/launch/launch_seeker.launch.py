eb22ef32ab0ab1b0ba0c249e6e8ee776a40f6b79
Parker <pknopf@ucsd.edu>
2022-03-05 21:09:19 -0800

    common launch file

--- packages/seeker/launch/launch_seeker.launch.py
+++ packages/seeker/launch/launch_seeker.launch.py
@@ -1,10 +1,63 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+import yaml
 
 def generate_launch_description():
-    seeker_package = 'seeker'
-    cap_node_name = 'centroid_node'
+    seeker_pkg = 'seeker'
+    actuator_pkg = 'ucsd_robocar_actuator2_pkg'
+    seeker_calibration_file = 'seeker_calibration.yaml'
+    act_calibration_file = 'adafruit_twist_calibration.yaml'
 
-    cap = LaunchDescription()
+    capture_node_name = 'fan_node'
+    centroid_node_name = 'centroid_name'
+    fan_node_name = 'fan_node'
+    actuator_node_name = 'adafruit_twist_node'
 
-    return cap
+    ld = LaunchDescription()
+
+    config_seeker = os.path.join(
+        get_package_share_directory(seeker_pkg),
+        'config',
+        seeker_calibration_file)
+
+    config_actuator = os.path.join(
+        get_package_share_directory(actuator_pkg),
+        'config',
+        act_calibration_file)
+
+    capture_node = Node(
+        package = seeker_pkg,
+        executable = capture_node_name,
+        output='screen',
+        parameters=[config_seeker]
+    )
+
+    centroid_node = Node(
+        package = seeker_pkg,
+        executable = centroid_node_name,
+        output='screen',
+        parameters=[config_seeker]
+    )
+
+    fan_node = Node(
+        package = seeker_pkg,
+        executable = fan_node_name,
+        output='screen',
+        parameters=[config_seeker]
+    )
+
+    act_node = Node(
+        package = actuator_pkg,
+        executable = actuator_node_name,
+        output='screen',
+        parameters=[config_actuator]
+    )
+    
+    ld.add_action(capture_node)
+    ld.add_action(centroid_node)
+    ld.add_action(fan_node)
+    ld.add_action(act_node)
+
+    return ld
