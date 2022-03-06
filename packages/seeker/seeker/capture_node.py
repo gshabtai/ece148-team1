67a234f3044573bb84e4abccacabf9364f5bca98
Parker <pknopf@ucsd.edu>
2022-03-05 17:53:46 -0800

    error scale

--- packages/seeker/seeker/capture_node.py
+++ packages/seeker/seeker/capture_node.py
@@ -70,7 +70,7 @@ class CaptureControl(Node):
 
     def computeCapture(self, data):
         # setting up PID control
-        self.ek = float(data.data[0] / 100)
+        self.ek = float(data.data[0] / 200)
 
         # Throttle gain scheduling (function of error)
         self.inf_throttle = self.min_throttle - (self.min_throttle - self.max_throttle) / (1 - self.error_threshold)
