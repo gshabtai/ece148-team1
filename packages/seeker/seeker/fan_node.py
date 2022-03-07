5737040adbad3d7f8b373507ca15e73fb68efbc9
Parker <pknopf@ucsd.edu>
2022-03-06 21:04:15 -0800

    1

--- packages/seeker/seeker/fan_node.py
+++ packages/seeker/seeker/fan_node.py
@@ -38,7 +38,7 @@ class AdafruitFan(Node):
     def send_values_to_adafruit(self, data):
         fan_power = data.linear.y
 
-        if (fan_power):
+        if (fan_power == 1):
             GPIO.output(self.fan1_channel, GPIO.HIGH)
         else:
             GPIO.output(self.fan1_channel, GPIO.LOW)
