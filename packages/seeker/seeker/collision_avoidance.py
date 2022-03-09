b0eddfdd27603fe98d9499c33fcaf51c8714a6dc
jacob <jbingham@ucsd.edu>
2022-03-09 13:58:50 -0800

    loop

--- packages/seeker/seeker/collision_avoidance.py
+++ packages/seeker/seeker/collision_avoidance.py
@@ -1,6 +1,7 @@
 import rclpy 
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
 import numpy as np
 
  
@@ -9,21 +10,40 @@ class CollisionAvoidance(Node):
         # call super() in the constructor in order to initialize the Node object with node name as only parameter
         super().__init__('counter_publisher')
         self.subscriber = self.create_subscription(LaserScan, '/scan', self.talker_callback,10)
+        self.twist_publisher = self.create_publisher(Twist, '/cmd_vel', 10)
 
  
     def talker_callback(self, data):
-        max_dist = 2
-        min_dist = .45
+        r_outer = 2
+        r_inner = .45
         filtered_data = data.ranges[270:359] + data.ranges[0:90]
         # to-do: optimization
         for num in filtered_data:
-            if num =< min_dist:
+            if num < r_inner:
                 filtered_data[filtered_data.index(num)] = NaN
 #        filtered_data = np.where(filtered_data < np.zeros((1,len(filtered_data))) + 0.45, filtered_data, 999)
-        minVal = filtered_data
+        minVal = min(filtered_data)
         index = filtered_data.index(minVal)
     
         self.get_logger().info("Angle: " + str(index) + ", Distance: " + str(minVal))
+
+      #  if minVal < r_outer:
+      #      steering_out(distance = minVal, angle = index)
+
+
+    def steering_out(self,distance,angle):
+        sensitivity = 1
+
+        # Publish values
+        try:
+            # publish control signals
+            #self.twist_cmd.linear.x = self.dyn_cmd.cal_throttle(self.ek)
+            self.twist_cmd.angular.z = angle + (1/distance)**sensitivity
+            self.twist_publisher.publish(self.twist_cmd)
+
+        except KeyboardInterrupt:
+            self.twist_cmd.linear.x = self.zero_throttle
+            self.twist_publisher.publish(self.twist_cmd)
             
 def main(args=None):
     rclpy.init(args=args) # initialize the ROS communication
