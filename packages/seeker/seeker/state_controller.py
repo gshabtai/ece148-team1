a49ec597a963a075e5cdaf1a317dbfb527695adb
Moises Lopez <mdlopezme@gmail.com>
2022-03-09 14:22:00 -0800

    publish state

--- packages/seeker/seeker/state_controller.py
+++ packages/seeker/seeker/state_controller.py
@@ -7,10 +7,27 @@ class StateController(Node):
     def __init__(self) -> None:
         super().__init__('state_controller')
         self.state_publisher = self.create_publisher(String, '/state', 10)
-        self.create_timer(0.2, self.next_state)
+        self.create_timer(0.2, self.update)
+        self.current_state = 'idle'
+        self.next_state = 'idle'
+        self.msg = String()
 
-    def next_state(self):
-        self.get_logger().info("Hello ROS2")
+        # Set starting params
+        self.number_loaded_ball = 0
+
+    def update(self):
+        self.next_state = self.calc_next_state()
+        self.msg.data = self.next_state
+        self.state_publisher(self.msg)
+
+    def calc_next_state(self):
+        if self.current_state == 'idle' and self.number_loaded_ball < 4:
+            return 'search_mode'
+        elif self.current_state == 'idle' and self.number_loaded_ball >= 4:
+            return 'idle'
+        
+        # Default parameter
+        return 'idle'
 
 
 def main(args=None):
