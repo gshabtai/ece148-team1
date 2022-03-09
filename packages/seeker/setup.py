634e75609d1f2c155493ef329f77bcabbfcc9d5b
Moises Lopez <mdlopezme@gmail.com>
2022-03-08 23:42:13 -0800

    disabled config

--- packages/seeker/setup.py
+++ packages/seeker/setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
             (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-            (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+            # (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
