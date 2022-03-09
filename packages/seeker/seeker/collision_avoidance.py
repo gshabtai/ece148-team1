98835d75beda0bf9508dd55656f9ece9dbc5c46d
jacob <jbingham@ucsd.edu>
2022-03-09 00:14:26 -0800

    so far just detects lidar

---
+++ packages/seeker/seeker/collision_avoidance.py
@@ -0,0 +1,25 @@
+import rclpy 
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+import numpy
+
+ 
+class CollisionAvoidance(Node):
+    def __init__(self):
+        # call super() in the constructor in order to initialize the Node object with node name as only parameter
+        super().__init__('counter_publisher')
+        self.subscriber = self.create_subscription(LaserScan, '/scan', self.talker_callback,10)
+
+ 
+    def talker_callback(self, data):
+        self.get_logger().info(numpy.size(data))
+            
+def main(args=None):
+    rclpy.init(args=args) # initialize the ROS communication
+    lidarInfoSize = CollisionAvoidance() # declare the node constructor
+    rclpy.spin(lidarInfoSize) # pause the program execution, waits for a request to kill the node (ctrl+c)
+    lidarInfoSize.destroy_node() # Explicitly destroy the node
+    rclpy.shutdown() # shutdown the ROS communication
+ 
+if __name__ == '__main__':
+    main()
