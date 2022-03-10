4c6667ae575b462ba5b5ae9bfe112504ad282023
jacob <jbingham@ucsd.edu>
2022-03-09 17:01:26 -0800

    third try

--- packages/seeker/seeker/collision_avoidance.py
+++ packages/seeker/seeker/collision_avoidance.py
@@ -9,6 +9,7 @@ from std_msgs.msg import Bool
 class CollisionAvoidance(Node):
     count = 0
     data_range = 10
+    collected_data_log = np.zeros(1,data_range)
 
     def __init__(self):
         # call super() in the constructor in order to initialize the Node object with node name as only parameter
