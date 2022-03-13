db6b5dff79d14eef88c21dff290ada11ae5fc353
Moises Lopez <mdlopezme@gmail.com>
2022-03-12 18:59:16 -0800

    a

--- packages/seeker/launch/state_machine_all.launch.py
+++ packages/seeker/launch/state_machine_all.launch.py
@@ -125,17 +125,17 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     # Dominic
-    # ld.add_action(webcam_node)
+    ld.add_action(webcam_node)
     ld.add_action(adafruit_node)
     ld.add_action(lidar_node)
 
     # Ours
-    # ld.add_action(webcam_publish_centroid_node)
+    ld.add_action(webcam_publish_centroid_node)
     ld.add_action(state_machine)
-    # ld.add_action(intake_system_node)
-    # ld.add_action(intel_launch)
-    # ld.add_action(intel_centroid)
-    # ld.add_action(capture_node)
+    ld.add_action(intake_system_node)
+    ld.add_action(intel_launch)
+    ld.add_action(intel_centroid)
+    ld.add_action(capture_node)
     ld.add_action(simples_states_node)
     ld.add_action(collision_avoidance_node)
 
