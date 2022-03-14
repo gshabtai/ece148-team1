65dadd06effbf6dea4bd955ded9ee1db78172c5c
Parker <pknopf@ucsd.edu>
2022-03-13 20:17:12 -0700

    new twist launch

---
+++ packages/seeker/launch/adafruit_twist.launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+import yaml
+
+def generate_launch_description():
+    # Define package names
+    pkg = 'ucsd_robocar_actuator2_pkg'
+    node_name = 'adafruit_twist_node'
+
+    # Define yaml config files
+    config_file = 'adafruit_twist_calibration.yaml'
+
+    # Lanuch Descriptions
+    ld = LaunchDescription()
+
+    config = os.path.join(
+        get_package_share_directory(pkg),
+        'config',
+        config_file)
+
+    adafruit_twist_node = Node(
+        package = pkg,
+        executable = node_name,
+        output = 'screen',
+        parameters = [config]
+    )
+    
+    # Add actions to launch description
+    ld.add_action(adafruit_twist_node)
+
+    return ld
