8a9c90fed0bec78425ba7aad32c973b23a491536
Parker <pknopf@ucsd.edu>
2022-03-13 21:51:30 -0700

    uncomment import

--- packages/seeker/launch/state_machine.launch.py
+++ packages/seeker/launch/state_machine.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-#from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory
 import os
 import yaml
 
